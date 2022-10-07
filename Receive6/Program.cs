fc202ec27c585168db802bec8abddbb391e9fe67
umeshnagarro <umesh.gupta@nagarro.com>
2022-10-07 20:03:34 +0530

    Initial commit

---
+++ Receive6/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+// See https://aka.ms/new-console-template for more information
+var factory = new ConnectionFactory() { HostName = "localhost" };
+factory.UserName = "user";
+factory.Password ="nrs-rocks";
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "MyTestQueue",
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "MyTestQueue",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
---
+++ Send6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+// See https://aka.ms/new-console-template for more information
+var factory = new ConnectionFactory() { HostName = "localhost"};
+factory.UserName = "user";
+factory.Password ="nrs-rocks";
+        using(var connection = factory.CreateConnection())
+    using(var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: "MyTestQueue",
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+        
+        for (int i = 1; i <= 100; i++)
+        {
+            Thread.Sleep(2000);
+            string message = $"Message no {i} at  {DateTime.Now}";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "",
+                                routingKey: "MyTestQueue",
+                                basicProperties: null,
+                                body: body);
+        Console.WriteLine(" [x] Sent {0}", message);
+        }
+        
+    }
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
